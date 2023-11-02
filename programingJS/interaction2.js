6083ad50f3c2c3fd13633ee68b8d6b7fd82ff6e0
Gabriel Alegre <gale.el22@gmail.com>
2023-11-02 14:37:57 -0300

    modificado para noticia

--- programingJS/interaction2.js
+++ programingJS/interaction2.js
@@ -1,12 +1,24 @@
 //tuve que crear un nuevo archivo js porque no comparten todas las funciones las 3 paginas.
 
-
-document.querySelector(".nav__responsive-button").addEventListener("click",() => {
+document
+  .querySelector(".nav__responsive-button")
+  .addEventListener("click", () => {
     document.querySelector(".nav__responsive").style.display = "flex";
-    document.querySelector(".back_menu").style.display = "flex"
-})
+    document.querySelector(".back_menu").style.display = "flex";
+  });
 
 document.querySelector(".back_menu").addEventListener("click", () => {
-    document.querySelector(".nav__responsive").style.display = "none";
-    document.querySelector(".back_menu").style.display = "none"
-})
+  document.querySelector(".nav__responsive").style.display = "none";
+  document.querySelector(".back_menu").style.display = "none";
+});
+
+document.getElementById("pa").addEventListener("click", () => {
+  document.getElementById("ro").style.display = "none";
+  document.querySelector(".container-video__pa").style.display = "block";
+});
+
+document.getElementById("ro").addEventListener("click", () => {
+  document.getElementById("pa").style.display = "none";
+  document.querySelector(".container-video__ro").style.display =
+    "block";
+});
